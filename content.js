c867367da15b183d7f324e9b1a07bfd5ecf4cbb7
hengyou zhang <hengyouzhang@microsoft.com>
2023-05-08 01:44:16 +0800

    add test run

--- content.js
+++ content.js
@@ -1,108 +1,166 @@
 function replaceText(text, find, regex, findRegex, replace) {
-    if (text == null) {
-        return null;
+  if (text == null) {
+    return null;
+  }
+  let findResult = null;
+  if (regex) {
+    findResult = findRegex.exec(text);
+  } else {
+    if (text.indexOf(find) != -1) {
+      findResult = new Array();
+      findResult.push(find);
     }
-    let findResult = null;
-    if (regex) {
-        findResult = findRegex.exec(text);
-    } else {
-        if (text.indexOf(find) != -1) {
-            findResult = new Array();
-            findResult.push(find);
-        }
+  }
+  if (findResult == null || findResult.length == 0) {
+    return null;
+  }
+  let realReplace = replace;
+  for (let k = 0; k < findResult.length; k++) {
+    const param = "$" + k;
+    if (realReplace.indexOf(param) != -1) {
+      realReplace = realReplace.replaceAll(param, findResult[k]);
     }
-    if ((findResult == null) || (findResult.length == 0)) {
-        return null;
-    }
-    let realReplace = replace;
-    for (let k = 0; k < findResult.length; k++) {
-        let param = '$' + k;
-        if (realReplace.indexOf(param) != -1) {
-            realReplace = realReplace.replaceAll(param, findResult[k]);
-        }
-    }
-    let result = new Array();
-    result.push(findResult[0]);
-    result.push(realReplace);
-    return result;
+  }
+  let result = new Array();
+  result.push(findResult[0]);
+  result.push(realReplace);
+  return result;
 }
 
-function replaceElementText(find, regex, replace) {
-    let elements = document.body.getElementsByTagName('*');
-    const findRegex = new RegExp(find, 'm');
-    for (let i = 0; i < elements.length; i++) {
-        let element = elements[i];
-        const tagName = element.tagName.toLowerCase();
-        if ((tagName == "script") || (tagName == "style") || (tagName == "img")) {
-            continue;
-        }
-        if (element.childNodes.length > 0) {
-            for (let j = 0; j < element.childNodes.length; j++) {
-                let node = element.childNodes[j];
-                if (node.nodeType === Node.TEXT_NODE) {
-                    let text = node.nodeValue;
-                    let result = replaceText(text, find, regex, findRegex, replace);
-                    if (result == null) {
-                        continue;
-                    }
-                    if (text.indexOf(result[1]) == -1) {
-                        let newText = text.replaceAll(result[0], result[1]);
-                        element.replaceChild(document.createTextNode(newText), node);
-                    }
-                }
-            }
-        } else if (element.tagName.toLowerCase() == "input") {
-            let text = element.value;
-            let result = replaceText(text, find, regex, findRegex, replace);
-            if (result == null) {
-                continue;
-            }
-            if (text.indexOf(result[1]) == -1) {
-                let newText = text.replaceAll(result[0], result[1]);
-                element.value = newText;
-            }
-        }
+function replaceElementText(find, regex, replace, check) {
+  const elements = document.body.getElementsByTagName("*");
+  let findRegex = null;
+  if (regex) {
+    try {
+      findRegex = new RegExp(find, "m");
+    } catch (e) {
+      return 0;
     }
+  }
+  let findCount = 0;
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    const tagName = element.tagName.toLowerCase();
+    if (tagName == "script" || tagName == "style" || tagName == "img") {
+      continue;
+    }
+    const visible = element.offsetWidth > 0 && element.offsetHeight > 0;
+    if (!visible) {
+      continue;
+    }
+    if (element.childNodes.length > 0) {
+      for (let j = 0; j < element.childNodes.length; j++) {
+        const node = element.childNodes[j];
+        if (node.nodeType === Node.TEXT_NODE) {
+          const text = node.nodeValue;
+          const result = replaceText(text, find, regex, findRegex, replace);
+          if (result == null) {
+            continue;
+          }
+          if (replace == result[1] || text.indexOf(result[1]) == -1) {
+            findCount = findCount + 1;
+            if (check == false) {
+              const newText = text.replaceAll(result[0], result[1]);
+              element.replaceChild(document.createTextNode(newText), node);
+            }
+          }
+        }
+      }
+    } else if (element.tagName.toLowerCase() == "input") {
+      const text = element.value;
+      const result = replaceText(text, find, regex, findRegex, replace);
+      if (result == null) {
+        continue;
+      }
+      if (replace == result[1] || text.indexOf(result[1]) == -1) {
+        findCount = findCount + 1;
+        if (check == false) {
+          const newText = text.replaceAll(result[0], result[1]);
+          element.value = newText;
+        }
+      }
+    }
+  }
+  return findCount;
 }
 
 function repeatReplace(times) {
-    if (times <= 4) {
-        setTimeout(function () {
-            chrome.storage.sync.get(null, function (result) {
-                for (let key in result) {
-                    let value = result[key];
-                    if ((value.domain != null) && (value.domain != window.location.host)) {
-                        continue;
-                    }
-                    if (value.runtype == "Manual") {
-                        continue;
-                    }
-                    replaceElementText(key, value.regex, value.replace);
-                }
-                repeatReplace(times + 1);
-            });
-        }, times * 1000);
-    }
+  if (times <= 4) {
+    setTimeout(function () {
+      chrome.storage.sync.get(null, function (result) {
+        for (let key in result) {
+          const value = result[key];
+          if (value.domain != null && value.domain != window.location.host) {
+            continue;
+          }
+          if (value.runtype == "Manual") {
+            continue;
+          }
+          replaceElementText(key, value.regex, value.replace, false);
+        }
+        repeatReplace(times + 1);
+      });
+    }, times * 1000);
+  }
 }
 
 function main() {
-    chrome.storage.local.get("cmd", function (result) {
-        if (result["cmd"] == null) {
-            repeatReplace(1);
-        } else {
-            let value = result["cmd"];
-            chrome.storage.sync.get(value.find, function (result) {
-                for (let key in result) {
-                    let value = result[key];
-                    if ((value.domain != null) && (value.domain != window.location.host)) {
-                        continue;
-                    }
-                    replaceElementText(key, value.regex, value.replace);
-                }
-                chrome.storage.local.remove("cmd");
-            });
-        }
-    });
+  chrome.storage.local.get("cmd", function (result) {
+    if (result["cmd"] == null) {
+      repeatReplace(1);
+    } else {
+      chrome.storage.local.remove("cmd");
+      const value = result["cmd"];
+      if (value.type == "once") {
+        chrome.storage.sync.get(value.find, function (result) {
+          let replaceCount = 0;
+          for (let key in result) {
+            const value = result[key];
+            if (value.domain != null && value.domain != window.location.host) {
+              continue;
+            }
+            replaceCount =
+              replaceCount +
+              replaceElementText(key, value.regex, value.replace, false);
+          }
+          chrome.runtime.sendMessage({ replaceCount: replaceCount });
+        });
+      } else if (value.type == "test") {
+        chrome.storage.local.get("tmp", function (result) {
+          const rule = result["tmp"];
+          if (rule == null || rule.valid == false) {
+            return;
+          }
+          const value = rule.value;
+          if (value.domain != null && value.domain != window.location.host) {
+            return;
+          }
+          replaceElementText(rule.key, value.regex, value.replace, false);
+        });
+      } else if (value.type == "check") {
+        chrome.storage.local.get("tmp", function (result) {
+          const rule = result["tmp"];
+          let findCount = 0;
+          if (rule == null || rule.valid == false) {
+            findCount = 0;
+          } else {
+            const value = rule.value;
+            if (value.domain != null && value.domain != window.location.host) {
+              findCount = 0;
+            } else {
+              findCount = replaceElementText(
+                rule.key,
+                value.regex,
+                value.replace,
+                true
+              );
+            }
+          }
+          chrome.runtime.sendMessage({ findCount: findCount });
+        });
+      }
+    }
+  });
 }
 
-main()
+main();
--- popup.js
+++ popup.js
@@ -1,19 +1,17 @@
 // global vars
-var g_rule_index = 0;
+var g_rule_index = 0; // auto increase every time updateRulesTable, ensure that the ID of the showing rule row is unique
 var g_edit_mode = false;
+var g_adding_rule = false;
+var g_last_save_time = 0; // cursor away or click test will save tmp rule, record last save time
+var g_received_frames = new Set();
+var g_find_count = 0;
+var g_input_timer = null;
+var g_replace_count = 0;
 
-function Rule(domain, find, regex, replace, runtype, display) {
-  this.domain = domain;
-  this.find = find;
-  this.regex = regex;
-  this.replace = replace;
-  this.runtype = runtype;
-  this.display = display;
-}
-
+/* helper functions */
 function cutString(str, len) {
   let str_length = 0;
-  let str_cut = '';
+  let str_cut = "";
   for (let i = 0; i < str.length; i++) {
     let a = str.charAt(i);
     str_length++;
@@ -22,7 +20,7 @@ function cutString(str, len) {
     }
     str_cut = str_cut.concat(a);
     if (str_length >= len) {
-      str_cut = str_cut.concat('...');
+      str_cut = str_cut.concat("...");
       return str_cut;
     }
   }
@@ -34,12 +32,95 @@ function cutString(str, len) {
 /*
 functions to process rules
 */
+function showAddRuleBox(key, value) {
+  if (value.domain == null) {
+    document.getElementById("domains_checkbox").checked = true;
+    document.getElementById("domains_text").value = "";
+  } else {
+    document.getElementById("domains_checkbox").checked = false;
+    document.getElementById("domains_text").value = value.domain;
+  }
+  document.getElementById("find_text").value = key;
+  if (value.regex == true) {
+    document.getElementById("regex_checkbox").checked = true;
+  } else {
+    document.getElementById("regex_checkbox").checked = false;
+  }
+  document.getElementById("replace_text").value = value.replace;
+  document.getElementById("runtype_select").value = value.runtype;
+  document.getElementById("add_rule_box").style.visibility = "visible";
+  g_adding_rule = true;
+}
+
+function hideAddRuleBox() {
+  g_edit_mode = false;
+  g_adding_rule = false;
+  document.getElementById("add_rule_box").style.visibility = "hidden";
+  chrome.storage.local.remove("tmp");
+}
+
+function currentAddingRule() {
+  let valid = true;
+  let checked = document.getElementById("domains_checkbox").checked;
+  let domain = null;
+  if (!checked) {
+    domain = document.getElementById("domains_text").value;
+  }
+  let find = document.getElementById("find_text").value;
+  if (!find || find.trim().length == 0) {
+    valid = false;
+  }
+  let regex = document.getElementById("regex_checkbox").checked;
+  let replace = document.getElementById("replace_text").value;
+  replace = replace != null ? replace : "";
+  let runtype = document.getElementById("runtype_select").value;
+  return {
+    valid: valid,
+    key: find,
+    value: { domain: domain, regex: regex, replace: replace, runtype: runtype },
+  };
+}
+
+function saveTmpRule(force) {
+  let current_time = Date.now();
+  if (current_time - g_last_save_time < 3000 && force == false) {
+    return;
+  }
+  g_last_save_time = current_time;
+  let rule = currentAddingRule();
+  chrome.storage.local.set({ tmp: rule });
+  chrome.storage.local.set({ edit_mode: g_edit_mode });
+}
+
+function updateFindCount() {
+  clearTimeout(g_input_timer);
+  g_input_timer = setTimeout(function () {
+    let rule = currentAddingRule();
+    chrome.storage.local.set({ tmp: rule });
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.storage.local.set(
+        { cmd: { type: "check", find: null } },
+        function () {
+          g_received_frames.clear();
+          g_find_count = 0;
+          chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id, allFrames: true },
+            files: ["content.js"],
+          });
+        }
+      );
+    });
+  }, 1000);
+}
+
 function runRule(key) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.storage.local.set({ cmd: { type: "once", find: key } }, function () {
+      g_received_frames.clear();
+      g_replace_count = 0;
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id, allFrames: true },
-        files: ["content.js"]
+        files: ["content.js"],
       });
     });
   });
@@ -52,22 +133,8 @@ function editRule(key) {
       // do nothing
     } else {
       g_edit_mode = true;
-      if (value.domain == null) {
-        document.getElementById("domains_checkbox").checked = true;
-        document.getElementById("domains_text").value = "";
-      } else {
-        document.getElementById("domains_checkbox").checked = false;
-        document.getElementById("domains_text").value = value.domain;
-      }
-      document.getElementById("find_text").value = value.find;
-      if (value.regex == true) {
-        document.getElementById("regex_checkbox").checked = true;
-      } else {
-        document.getElementById("regex_checkbox").checked = false;
-      }
-      document.getElementById("replace_text").value = value.replace;
-      document.getElementById("runtype_select").value = value.runtype;
-      document.getElementById("add_rule_box").style.visibility = "visible";
+      showAddRuleBox(key, value);
+      updateFindCount();
     }
   });
 }
@@ -85,75 +152,114 @@ function updateRulesTable() {
     let map = new Map();
     for (let key in result) {
       g_rule_index++;
-      const runButton = 'run_button_' + g_rule_index;
-      const editButton = 'edit_button_' + g_rule_index;
-      const deleteButton = 'delete_button_' + g_rule_index;
-      map.set(key, { 'run': runButton, 'edit': editButton, 'delete': deleteButton });
-      innerHtml = innerHtml + `<tr><td>${result[key].display}</td><td><button id=\"${runButton}\" class=\"pure-button button-success\">Run</button></td><td><button id=\"${editButton}\" class=\"pure-button button-secondary\">Edit</button></td><td><button id=\"${deleteButton}\" class=\"pure-button button-error\">Delete</button></td></tr>`;
+      const runButton = "run_button_" + g_rule_index;
+      const editButton = "edit_button_" + g_rule_index;
+      const deleteButton = "delete_button_" + g_rule_index;
+      const display = cutString(key, 32);
+      map.set(key, { run: runButton, edit: editButton, delete: deleteButton });
+      innerHtml =
+        innerHtml +
+        `<tr><td>${display}</td><td><button id=\"${runButton}\" class=\"pure-button button-success\">Run</button></td><td><button id=\"${editButton}\" class=\"pure-button button-secondary\">Edit</button></td><td><button id=\"${deleteButton}\" class=\"pure-button button-error\">Delete</button></td></tr>`;
     }
     document.getElementById("rule_table").innerHTML = innerHtml;
     // add event lisenter after added to dom
     for (let [key, value] of map) {
-      document.getElementById(value['run']).addEventListener('click', function () {
-        runRule(key);
-      });
-      document.getElementById(value['edit']).addEventListener('click', function () {
-        editRule(key);
-      });
-      document.getElementById(value['delete']).addEventListener('click', function () {
-        deleteRule(key);
-      });
+      document
+        .getElementById(value["run"])
+        .addEventListener("click", function () {
+          runRule(key);
+        });
+      document
+        .getElementById(value["edit"])
+        .addEventListener("click", function () {
+          editRule(key);
+        });
+      document
+        .getElementById(value["delete"])
+        .addEventListener("click", function () {
+          deleteRule(key);
+        });
     }
   });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   updateRulesTable();
 
   // add rule button
-  document.getElementById("add_rule_button").addEventListener('click', function () {
-    chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
-      let url = tabs[0].url;
-      let domain = new URL(url).hostname;
-      document.getElementById("domains_text").value = domain;
+  document
+    .getElementById("add_rule_button")
+    .addEventListener("click", function () {
+      showAddRuleBox("", {
+        domain: "",
+        regex: false,
+        replace: "",
+        runtype: "Auto",
+      });
+      chrome.tabs.query(
+        { active: true, lastFocusedWindow: true },
+        function (tabs) {
+          let url = tabs[0].url;
+          let domain = new URL(url).hostname;
+          document.getElementById("domains_text").value = domain;
+        }
+      );
     });
-    document.getElementById("add_rule_box").style.visibility = "visible";
-  });
 
   // run all button
-  document.getElementById("run_all_button").addEventListener('click', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.storage.local.set({ cmd: { type: "once", find: null } }, function () {
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id, allFrames: true },
-          files: ["content.js"]
-        });
+  document
+    .getElementById("run_all_button")
+    .addEventListener("click", function () {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.storage.local.set(
+          { cmd: { type: "once", find: null } },
+          function () {
+            g_received_frames.clear();
+            g_replace_count = 0;
+            chrome.scripting.executeScript({
+              target: { tabId: tabs[0].id, allFrames: true },
+              files: ["content.js"],
+            });
+          }
+        );
       });
+    });
+
+  // test rule button
+  document.getElementById("test_button").addEventListener("click", function () {
+    let rule = currentAddingRule();
+    if (rule.valid == false) {
+      alert("Find is empty");
+      return;
+    }
+    saveTmpRule(true);
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.storage.local.set(
+        { cmd: { type: "test", find: null } },
+        function () {
+          chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id, allFrames: true },
+            files: ["content.js"],
+          });
+          updateFindCount();
+        }
+      );
     });
   });
 
   // save rule button
-  document.getElementById('save_button').addEventListener('click', function () {
-    let checked = document.getElementById("domains_checkbox").checked;
-    let domain = null;
-    if (!checked) {
-      domain = document.getElementById("domains_text").value;
-    }
-    let find = document.getElementById("find_text").value;
-    if (!find || (find.trim().length == 0)) {
+  document.getElementById("save_button").addEventListener("click", function () {
+    let rule = currentAddingRule();
+    if (rule.valid == false) {
+      alert("Find is empty");
       return;
     }
-    let regex = document.getElementById("regex_checkbox").checked;
-    let display = cutString(find, 32);
-    let replace = document.getElementById("replace_text").value;
-    let runtype = document.getElementById("runtype_select").value;
 
-    chrome.storage.sync.get([find], function (result) {
-      if (g_edit_mode || (result[find] == null)) {
-        chrome.storage.sync.set({ [find]: new Rule(domain, find, regex, replace, runtype, display) });
+    chrome.storage.sync.get([rule.key], function (result) {
+      if (g_edit_mode || result[rule.key] == null) {
+        chrome.storage.sync.set({ [rule.key]: rule.value });
         updateRulesTable();
-        g_edit_mode = false;
-        document.getElementById("add_rule_box").style.visibility = "hidden";
+        hideAddRuleBox();
       } else {
         alert("Duplicate find, save failed!");
       }
@@ -161,14 +267,64 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // cancel button
-  document.getElementById("cancel_button").addEventListener('click', function () {
-    document.getElementById("domains_checkbox").checked = false;
-    document.getElementById("domains_text").value = "";
-    document.getElementById("find_text").value = "";
-    document.getElementById("regex_checkbox").checked = false;
-    document.getElementById("replace_text").value = "";
-    document.getElementById("runtype_select").value = "Auto";
-    g_edit_mode = false;
-    document.getElementById("add_rule_box").style.visibility = "hidden";
+  document
+    .getElementById("cancel_button")
+    .addEventListener("click", function () {
+      hideAddRuleBox();
+    });
+
+  // save tmp rule
+  document
+    .getElementById("add_rule")
+    .addEventListener("mouseleave", function () {
+      if (g_adding_rule == false) {
+        return;
+      }
+      saveTmpRule(false);
+    });
+
+  // detect find value change
+  document.getElementById("find_text").onkeyup = function () {
+    updateFindCount();
+  };
+  document
+    .getElementById("regex_checkbox")
+    .addEventListener("change", function () {
+      updateFindCount();
+    });
+
+  // show add rule if has tmp
+  chrome.storage.local.get("tmp", function (result) {
+    if (result["tmp"] != null) {
+      showAddRuleBox(result["tmp"].key, result["tmp"].value);
+      updateFindCount();
+      chrome.storage.local.get("edit_mode", function (result) {
+        const value = result["edit_mode"];
+        if (value != null) {
+          g_edit_mode = value;
+        }
+      });
+    }
   });
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (g_received_frames.has(sender.frameId) == false) {
+    g_received_frames.add(sender.frameId);
+
+    // check find count
+    let findCount = request.findCount;
+    if (findCount != null) {
+      g_find_count = g_find_count + findCount;
+      document.getElementById("find_count").innerText = "Find " + g_find_count;
+    }
+
+    // check replace count
+    let replaceCount = request.replaceCount;
+    if (replaceCount != null) {
+      g_replace_count = g_replace_count + replaceCount;
+      document.getElementById("details").innerText =
+        g_replace_count + " places were replaced";
+    }
+  }
 });
